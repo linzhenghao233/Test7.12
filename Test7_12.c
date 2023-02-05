b81edf2137eb672bc1c1e7b727336a3aecd3b7c8
林政浩 <1392872316@qq.com>
2023-02-05 20:39:42 +0800

    0205

--- Test7_12.c
+++ Test7_12.c
@@ -1,20 +1,44 @@
 #include <stdio.h>
 
 //1.
-char ch;
-int blank = 0,linefeed = 0, other = 0;
+//char ch;
+//int blank = 0,linefeed = 0, other = 0;
+//
+//int main(void) {
+//	printf("输入一段字符，程序会计算空格数、换行符数和其它字符数并显示。输入#结束程序\n");
+//	while ((ch = getchar()) != '#') {
+//		if (ch == ' ')
+//			blank++;
+//		else if (ch == '\n')
+//			linefeed++;
+//		else
+//			other++;
+//	}
+//	printf("空格数：%d\n换行符数：%d\n其它字符数：%d", blank, linefeed, other);
+//
+//	return 0;
+//}
 
+//2.
+//int main(void) {
+//	char ch;
+//	int count;
+//	count = 0;
+//
+//	printf("输入字符，程序显示其对应的ASCII码（输入“#”结束程序）：");
+//	while ((ch = getchar()) != '#') {
+//		printf("%c：%d\t", ch, ch);
+//		count++;
+//		if (count % 8 == 0)
+//			printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+//3.
 int main(void) {
-	printf("输入一段字符，程序会计算空格数、换行符数和其它字符数并显示。输入#结束程序\n");
-	while ((ch = getchar()) != '#') {
-		if (ch == ' ')
-			blank++;
-		else if (ch == '\n')
-			linefeed++;
-		else
-			other++;
-	}
-	printf("空格数：%d\n换行符数：%d\n其它字符数：%d", blank, linefeed, other);
+
 
 	return 0;
 }
